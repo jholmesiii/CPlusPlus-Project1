bfceefe32dad323fbfb9da1aac2dd8ed9ff87b81
jholmesiii <jholmesiii@gmail.com>
2022-12-11 14:23:57 -0600

    Add files via upload
    Calculate final price of car based on base price, options, and sales tax

---
+++ CarCalculation.cpp
@@ -0,0 +1,37 @@
+// CarCalculation.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double salesTax = 6.5;
+    double basePrice, options, totalPrice, subTotal, taxedAmount;
+
+    cout << "What is the base price?\n" << "$";
+    cin >> basePrice;
+    cout << "How much are the options?\n" << "$";
+    cin >> options;
+
+    subTotal = basePrice + options;
+    taxedAmount = (salesTax / 100) * subTotal;
+    totalPrice = subTotal + taxedAmount;
+
+    cout << "\nThe total price is: $" << totalPrice << "\n";
+    /*Subtotal = Base + Total Options
+    Amount of Tax = Sales Tax / 100 * subtotal
+    Total Price = Subtotal + Amount of Tax*/
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
